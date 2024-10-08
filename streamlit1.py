f0948b846cd94939f2f858cbbf07d04a0bfd1d3d
n1dlex <q2831457@gmail.com>
2024-10-08 12:13:12 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -265,7 +265,7 @@ def main():
                 </div>
             """, unsafe_allow_html=True)
 
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
         
         with col1:
             # ARPU and cost per MB
